d2febd9b2dbb1cc2694455321895ed3a842e010c
jacinoberto <estudo.jacimaranoberto@gmail.com>
2025-03-30 22:08:08 -0300

    Adicionando o Swagger em produção

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -52,7 +52,7 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
